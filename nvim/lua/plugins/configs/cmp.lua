09205ccd51ce5e7103596bfcf07308c6e4036abd
Sergey Anokhin <elrocie@gmail.com>
2025-06-22 01:11:19 +0500

    feat: add minuet and avante sources to cmp configuration

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -105,7 +105,9 @@ local options = {
     }),
   },
   sources = {
-    { name = "copilot", group_index = 1 },
+    { name = "minuet", group_index = 2 },
+    { name = "avante", group_index = 2 },
+    { name = "copilot", group_index = 2 },
     { name = "nvim_lsp", group_index = 2 },
     { name = "luasnip", group_index = 2 },
     { name = "buffer", group_index = 2 },
