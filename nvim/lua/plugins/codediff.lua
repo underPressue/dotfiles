95d6a9346a9cb1b21319cfcb47335a160bc07361
Sergey Anokhin <elrocie@gmail.com>
2026-01-13 22:39:14 +0500

    chore: new shortcut to codediff

--- nvim/lua/plugins/codediff.lua
+++ nvim/lua/plugins/codediff.lua
@@ -2,5 +2,8 @@ return {
   "esmuellert/codediff.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
   cmd = "CodeDiff",
+  keys = {
+    { "<leader>gd", "<cmd>CodeDiff<cr>", desc = "CodeDiff" }
+  },
   opts = {}
 }
