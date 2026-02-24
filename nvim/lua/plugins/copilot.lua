a52d386f5950f14d52ab1e2006e5cf4e656aa182
Sergey Anokhin <elrocie@gmail.com>
2026-02-24 23:12:31 +0500

    nvim config update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,30 @@
 return {
-  enabled = true,
-  "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
-  opts = {
-    suggestion = {
-      enable = false,
+  {
+    "folke/sidekick.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    dependencies = { "github/copilot.vim" },
+    opts = {},
+    keys = {
+      {
+        "<tab>",
+        function()
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>"
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      { "<c-.>", function() require("sidekick.cli").toggle() end, desc = "Toggle Sidekick CLI" },
+      { "<leader>aa", function() require("sidekick.cli").toggle() end, desc = "Toggle Sidekick CLI" },
+      { "<leader>as", function() require("sidekick.cli").select() end, desc = "Select CLI tool" },
+      { "<leader>ap", function() require("sidekick.cli").prompt() end, desc = "Select prompt" },
     },
-    panel = {
-      enable = false,
-    },
+  },
+  {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "VeryLazy",
   },
 }
